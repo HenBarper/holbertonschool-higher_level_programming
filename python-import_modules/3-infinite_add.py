6a3d8dd1f3827f2afb928fce8c72f0d8b4123bd9
root <benjaminitonio@gmail.com>
2023-05-10 15:51:16 -0700

    3 v1

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    result = 0
+    i = 0
+
+    for argument in sys.argv:
+        if i != 0:
+            result += int(argument)
+        i += 1
+    print(result)
