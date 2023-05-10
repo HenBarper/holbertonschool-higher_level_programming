4d104c6e3733bd18f86b38bfb51f92fec5ce52f8
root <benjaminitonio@gmail.com>
2023-05-10 12:05:54 -0700

    9 v1

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    lastDigit = 0
+
+    if(number > 0):
+        lastDigit = number % 10
+    elif(number < 0):
+        lastDigit = number % -10
+    else:
+        pass
+
+    return lastDigit
