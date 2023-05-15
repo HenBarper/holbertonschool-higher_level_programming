5c9c760490b4b6e0215ac368bdbad0283fda5769
root <benjaminitonio@gmail.com>
2023-05-15 12:03:29 -0700

    9 v2

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if((len(my_list)) == 0):
         return None
     else:
-        bigNum = 0
+        bigNum = my_list[0]
         for i in my_list:
             if(i > bigNum):
                 bigNum = i
