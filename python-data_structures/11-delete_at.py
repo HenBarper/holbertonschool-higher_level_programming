ba6796cdd52080008d5090cbf0dad4950c4f353d
root <benjaminitonio@gmail.com>
2023-05-15 12:50:30 -0700

    11 v1

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    list_len = len(my_list)
+
+    if(idx < 0 or idx > list_len):
+        return my_list
+
+    my_list.remove(idx)
+    return my_list
