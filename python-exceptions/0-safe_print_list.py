953c85cdec298b20ad43607b0750a63748b3381d
root <benjaminitonio@gmail.com>
2023-05-16 16:40:10 -0700

    1 v1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    j = 0
+    count = 0
     for i in my_list:
         i -= 1
         try:
             if(i < x):
                 print("{}".format(my_list[i]), end='')
-                j += 1
+                count += 1
         except ValueError:
             pass
     print()
-    return j
+    return count
