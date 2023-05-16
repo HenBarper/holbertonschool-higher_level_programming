facf6c59a537369c24f83b8e0a614b1d62a4a2d4
root <benjaminitonio@gmail.com>
2023-05-16 12:11:16 -0700

    10 v2

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,8 +3,9 @@ def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
-    highscore = list(a_dictionary.values())[0]
+    highscore = 0
     theKey = None
+
     for key, value in a_dictionary.items():
         if value > highscore:
             highscore = value
