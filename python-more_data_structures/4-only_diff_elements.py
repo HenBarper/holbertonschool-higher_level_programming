f20b8f6fa9abbe4a77b814393400da598b507116
root <benjaminitonio@gmail.com>
2023-05-16 10:04:28 -0700

    4 v4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,5 +2,5 @@
 def only_diff_elements(set_1, set_2):
     new_set = set_1.difference(set_2)
     newer_set = set_2.difference(set_1)
-    newest_set = set_1.union(set_2)
+    newest_set = new_set.union(newer_set)
     return newest_set
