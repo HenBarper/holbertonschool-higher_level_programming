93d1b6e09131185d8e49e49b0ae24cd6072c39b7
root <benjaminitonio@gmail.com>
2023-05-16 16:32:01 -0700

    0 v2

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,11 @@ def safe_print_list(my_list=[], x=0):
     j = 0
     for i in my_list:
         i -= 1
-        if(i < x and my_list[i] is not None):
-            print("{}".format(my_list[i]), end='')
-            j += 1
+        try:
+            if(i < x):
+                print("{}".format(my_list[i]), end='')
+                j += 1
+        except ValueError:
+            pass
     print()
     return j
