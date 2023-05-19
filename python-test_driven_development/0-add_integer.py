1e6769797ec19a1b6b303e61a9e69f9c5891b45b
root <benjaminitonio@gmail.com>
2023-05-19 12:12:58 -0700

    0 v6

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -18,9 +18,9 @@ def add_integer(a, b=98):
         The result of the operation
     """
 
-    if a is None or type(a) not in [int, float] or a is not a or abs(a) > 1.7976931348623158e+308:
+    if a is None or type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if b is None or type(b) not in [int, float] or b is not b or abs(b) > 1.7976931348623158e+308:
+    if b is None or type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
