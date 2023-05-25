ef3b781d5e7fb9bf8b4c2d55d101371a103736c3
root <benjaminitonio@gmail.com>
2023-05-25 16:41:36 -0700

    0 v3

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-Write a function that returns the list 
+Write a function that returns the list
     of available attributes and methods of an object:
 
 Prototype: def lookup(obj):
 Returns a list object
 You are not allowed to import any module
 """
+
 
 def lookup(obj):
     """Lookup functions returns a list casted dir() of the object"""
