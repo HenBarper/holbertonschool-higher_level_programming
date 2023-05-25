069d21c345c08ca6f8b6ee7b267007570d02760a
root <benjaminitonio@gmail.com>
2023-05-25 10:37:46 -0700

    19 v2

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-"""Returns a copy of a list"""
-
-
 def copy_list(a_list):
-    """Make a copy that list"""
-    new_list = a_list.copy()
-
-    return new_list
+    return a_list.copy()
