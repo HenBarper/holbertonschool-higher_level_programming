8f15bca3d821693a930db0615d8c989372bb93d7
root <benjaminitonio@gmail.com>
2023-05-25 13:56:44 -0700

    101 v1

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -2,3 +2,11 @@
 def magic_string():
     setattr(magic_string, 'count', getattr(magic_string, 'count', 0) + 1)
     return 'BestSchool' + ', BestSchool' * (magic_string.count - 1)
+
+#Previous implementation
+# def magic_string():
+#     if not hasattr(magic_string, 'count'):
+#         magic_string.count = 1
+#     else:
+#         magic_string.count += 1
+#     return 'BestSchool' + ', BestSchool' * (magic_string.count - 1)
--- python-everything_is_object/test.py
+++ python-everything_is_object/test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 a = (1,)
 
+print(id(a))
 print(type(a))
