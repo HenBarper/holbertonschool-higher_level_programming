c9b2cd7da9dbe5493d3f552604fb2a64a91356dc
root <benjaminitonio@gmail.com>
2023-05-25 14:04:04 -0700

    101 v2

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""DOCUMENTED CODE IS HERE"""
+
 
 class LockedClass:
+    """DOCUMENTED CLASS IS HERE"""
     __slots__ = ['first_name']
 
     def __setattr__(self, attr, value):
+        """SET THE ATTR BUT ONLY IS FIRST_NAME"""
+
         if not hasattr(self, attr) and attr != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(attr))
+            raise AttributeError("'LockedClass' object has \
+                no attribute '{}'".format(attr))
         super().__setattr__(attr, value)
