ed0cf4ec90b8262d1e4509d268701fdf086d0f1c
root <benjaminitonio@gmail.com>
2023-05-26 13:34:59 -0700

    9 v1

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Write a class Rectangle that inherits from BaseGeometry
+
+Instantiation with width and height: def __init__(self, width, height)::
+width and height must be private. No getter or setter
+width and height must be positive integers validated by integer_validator
+the area() method must be implemented
+print() should print, and str() should return, the following rectangle
+    description: [Rectangle] <width>/<height>
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    This is a Rectangle class!
+    """
+    def __init__(self, width, height):
+        """
+        This is the Initialization Method
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self
