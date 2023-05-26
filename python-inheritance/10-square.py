76fe49a0fd66b304e2cb92e67e45385a5687bbb4
root <benjaminitonio@gmail.com>
2023-05-26 14:13:24 -0700

    10 v2

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -13,13 +13,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     def __init__(self, size):
         """This is the Initialization Method"""
+        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        """returns area of square"""
-        return (self.__size ** 2)
-
-    def __str__(self):
-        """return string representation of rectangle"""
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
