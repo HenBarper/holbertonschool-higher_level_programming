a6360ce0a931bd97f9d60d0ac46b6b0d2f94691e
root <benjaminitonio@gmail.com>
2023-05-25 20:29:55 -0700

    1 v6

--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -2,11 +2,9 @@
 MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
+my_list.append('I')
+my_list.append('Like')
+my_list.append('Peanut Butter')
 print(my_list)
 my_list.print_sorted()
 print(my_list)
