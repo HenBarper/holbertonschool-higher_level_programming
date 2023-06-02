3f3a48f1f6d35b878240c84f354e6167e0e6b329
root <benjaminitonio@gmail.com>
2023-06-02 13:22:53 -0700

    13 v1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -118,3 +118,7 @@ class Rectangle(Base):
                 setattr(self, attr_names[i], arg)
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Function to return dict def of Rect"""
+        return {'id': self.id, 'width': self.__width, 'height': self.__height, 'x': self.__x, 'y': self.y}
