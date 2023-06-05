703c8472e8c22b65a8794665833b8b0f70466a88
root <benjaminitonio@gmail.com>
2023-06-05 10:38:17 -0700

    0 v7

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,3 +1,4 @@
+
 import unittest
 from models.base import Base
 
@@ -12,11 +13,6 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base1.id, 1)
         self.assertEqual(base2.id, 2)
         self.assertEqual(base3.id, 100)
-
-    def test_id_type(self):
-        '''test the type of id attribute to ensure an integer'''
-        base = Base(10)
-        self.assertIsInstance(base.id, int)
 
 if __name__ == '__main__':
     unittest.main()
