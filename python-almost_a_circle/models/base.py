cacc0c20bd9542c45811e79fd4bfbbfc5846552d
root <benjaminitonio@gmail.com>
2023-06-04 17:47:24 -0700

    17 v4

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -49,5 +49,4 @@ class Base():
         """returns list of json string data"""
         if json_string is None or len(json_string) == 0:
             return []
-        return json.dumps(json_string)
-
+        return json.loads(json_string)
