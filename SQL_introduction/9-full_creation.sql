0a7d904756570c70550b374f7f8d5f112f119b8e
root <benjaminitonio@gmail.com>
2023-06-23 13:51:38 -0700

    9 v6

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@ CREATE table IF NOT EXISTS second_table (
 
 INSERT INTO second_table
 VALUES
-(1, "John", 10),
-(2, "Alex", 3),
-(3, "Bob", 14),
-(4, "George", 8);
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
