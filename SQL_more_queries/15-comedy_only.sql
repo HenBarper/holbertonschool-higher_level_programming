3acfb684369041700eb70fde8fb6cb0a9f543466
root <benjaminitonio@gmail.com>
2023-06-27 15:44:39 -0700

    15 v2

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -3,5 +3,5 @@ SELECT tv_shows.title
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_genres = "Comedy"
+WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title;
