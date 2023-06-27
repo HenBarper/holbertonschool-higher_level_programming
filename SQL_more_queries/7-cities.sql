7945d161272093b564cf9ae09c5ae84eefc6c9d9
root <benjaminitonio@gmail.com>
2023-06-27 09:29:21 -0700

    7 v1

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    state_id INT AUTO_INCREMENT FOREIGN KEY(id) NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
