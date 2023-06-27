abfeee90ef5308503dab0641ad3844cb3afbb0d4
root <benjaminitonio@gmail.com>
2023-06-27 11:09:30 -0700

    12 v2

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 11-genre_id_all_shows.sql)
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-Right JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
