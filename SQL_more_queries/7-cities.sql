e06743417bc1ed233dc6df1c68fe92900b425ba4
root <benjaminitonio@gmail.com>
2023-06-27 09:31:13 -0700

    7 v2

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 use hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    state_id INT AUTO_INCREMENT FOREIGN KEY(id) NOT NULL,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
