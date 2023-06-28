205756481368b5fa65d14145dbf65ad19eca0d0e
root <benjaminitonio@gmail.com>
2023-06-28 08:13:46 -0700

    4 v4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,9 @@ def list_cities():
 
     cur = db.cursor()
 
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY id")
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities LEFT JOIN states ON states.id =\
+                cities.state_id ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
