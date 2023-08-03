2d2802d1b286fd4d8142d0caf5604f1e7d5d6329
Ben Harper <benjaminitonio@gmail.com>
2023-08-03 15:08:03 -0500

    0 v2

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 const ARGV = process.argv;
 
 fs.readFile(ARGV[2], 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(data);
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
 });
