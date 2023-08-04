057ed183ddfcbb335fce5f75037016a95e724f2a
Ben Harper <benjaminitonio@gmail.com>
2023-08-04 13:34:48 -0500

    4 v3

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -12,7 +12,7 @@ request.get(url, (err, response, body) => {
 
   if (response.statusCode === 200) {
     const data = JSON.parse(body);
-    console.log(data.films.length);
+    console.log((data.films).length);
   } else {
     console.error('Request failed with status:', response.statusCode);
   }
